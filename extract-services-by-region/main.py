51258c1177b4c3aa2eea1bf35027b2f1d57a51a1
camillehe1992 <camille.he@outlook.com>
2023-08-06 15:11:28 +0800

    chore: extract target regions content

--- extract-services-by-region/main.py
+++ extract-services-by-region/main.py
@@ -1,11 +1,31 @@
+import json
 import requests
+
+
+REGIONS = ["cn-north-1", "cn-northwest-1", "eu-central-1"]
+
+
+def extract_regions(content: dict) -> dict:
+    result = []
+    for item in content.get("prices"):
+        if item["id"].split(":").pop() in REGIONS:
+            result.append(item["attributes"])
+    return result
 
 
 def lambda_handler(event, context):
     response = requests.get("https://api.regional-table.region-services.aws.a2z.com")
     content = response.json()
+    extracted_content = extract_regions(content)
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": content,
+        "body": extracted_content,
     }
+
+
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    response = lambda_handler({}, {})
+    print(json.dumps(response, indent=2))
